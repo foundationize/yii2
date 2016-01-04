483559bd0b718bd90fb295f678b9c10d7a960d63
Gert van Tonder <gvantonder@ampco.com.au>
2016-01-05 09:17:05 +1100

    Point FoundationAsset to vendor package.

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -24,7 +24,6 @@ class AppAsset extends AssetBundle
     ];
     public $depends = [
         'yii\web\YiiAsset',
-        'app\assets\FoundationAsset',
-        //'yii\bootstrap\BootstrapAsset',
+        'foundationize\foundation\FoundationAsset', // in with the new        
     ];
 }
