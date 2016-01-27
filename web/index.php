8209da239647cfad365ee14142e1b39f00aa264c
gvanto <gvanto@gmail.com>
2016-01-28 10:01:08 +1100

    Added check for index-dev.php. web/index-dev.php added to .gitignore.

--- web/index.php
+++ web/index.php
@@ -1,8 +1,20 @@
 <?php
 
-// comment out the following two lines when deployed to production
-defined('YII_DEBUG') or define('YII_DEBUG', true);
-defined('YII_ENV') or define('YII_ENV', 'dev');
+// If index-dev exists, load it's contents
+if (file_exists(dirname(__FILE__) . '/index-dev.php')) {
+    include dirname(__FILE__) . '/index-dev.php'; 
+    
+    /*
+     * In your local dev environment: 
+      - Create a file 'index-dev.php'
+      - Add following to file:
+        defined('YII_DEBUG') or define('YII_DEBUG', true);
+        defined('YII_ENV') or define('YII_ENV', 'dev');
+      
+      - Add file to .gitignore (or however you ignore it from source control)
+        this way when deploying to live/staging, index-dev doesn't get deployed 
+     */    
+}
 
 require(__DIR__ . '/../vendor/autoload.php');
 require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
