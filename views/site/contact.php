58f824ab21764a1604b85cd704aae6019d3e4f6b
gvanto <gvanto@gmail.com>
2016-01-27 12:02:30 +1100

    Updated contact form success message class.

--- views/site/contact.php
+++ views/site/contact.php
@@ -21,7 +21,7 @@ $this->params['breadcrumbs'][] = $this->title;
 
     <?php if (Yii::$app->session->hasFlash('contactFormSubmitted')): ?>
 
-        <div class="alert alert-success">
+        <div class="callout success">
             Thank you for contacting us. We will respond to you as soon as possible.
         </div>
 
