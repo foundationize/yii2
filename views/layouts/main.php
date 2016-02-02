472a19cd82d6fefc63d81f2c2d2d4578bccf277a
gvanto <gvanto@gmail.com>
2016-02-02 14:30:52 +1100

    Added Google Analytics tracking JS (replace with your own GA).

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -186,12 +186,20 @@ AppAsset::register($this);
     <?php
     // Google Anlaytics: 
     // Replace the below with your own Analytics / tracking JS
-    if (!YII_ENV_DEV && !YII_ENV_TEST && strpos($_SERVER["SERVER_NAME"], 'yii2.foundationize.com') !== false) {
-        \app\models\Utils::showPre("DBG: Live server_name=" . $_SERVER["SERVER_NAME"]);
-    } else {
-        \app\models\Utils::showPre("DBG: server_name=" . $_SERVER["SERVER_NAME"]);
-    }
-    ?>
+    if (!YII_ENV_DEV && !YII_ENV_TEST && strpos($_SERVER["SERVER_NAME"], 'yii2.foundationize.com') !== false) : ?>
+    <?php    //\app\models\Utils::showPre("DBG: Live server_name=" . $_SERVER["SERVER_NAME"]); ?>
+    
+    <script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-73221432-1', 'auto');
+    ga('send', 'pageview');
+    </script>
+    
+    <?php endif; ?>
     
 </body>
 </html>
